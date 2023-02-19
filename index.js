68c2aaa10a5a1ed4fd7394007ade1f4f4bc531d3
williamelliott <ironmaiden229@hotmail.com>
2023-02-18 21:36:16 -0500

    finished project

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+    return a + b ;
+}   
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b
+}
+function increment(n){
+    var m = n + 1
+    return m
+}
+function decrement(n){
+    var m = n - 1
+    return m
+}
+function makeInt(n){
+    return parseInt(n, 10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
